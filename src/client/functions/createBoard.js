24650181572fb516519ae20fb07eb31535c0653d
Maxime JOUBERT <mjoubert@e1r7p8.42.fr>
2020-02-07 16:51:02 +0100

    Test

--- src/client/functions/createBoard.js
+++ src/client/functions/createBoard.js
@@ -3,7 +3,7 @@ export const HEIGHT = 19
 
 const createBoard = () => {
     let board = Array.from(Array(HEIGHT), () => {
-      return new Array(WIDTH).fill(0)
+      return new Array(WIDTH).fill(0, 'clear')
     })
     return board 
 }
