47c7df165c455cf5c0c1b3419a32f57eebe75ddd
Maxime JOUBERT <m.joubert.92@gmail.com>
2020-03-14 17:59:49 +0100

    push

--- src/client/hook/tests/usePlayer.spec.js
+++ src/client/hook/tests/usePlayer.spec.js
@@ -1,21 +1,90 @@
+import React from 'react'
 import { usePlayer } from '../usePlayer'
 import { renderHook, act } from '@testing-library/react-hooks';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import reducer from '../../reducers'
 
-it('should init usePlayer', () => {
-  const { result } = renderHook(() => usePlayer());
+const initialState = {
+  game : {
+    shapes : [
+      {
+        shape: [
+          ["I", "I", "I", "I"],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0]
+        ]
+      }
+    ]
+  }
+}
 
-  console.log(result.current[0], '------');
-  // expect(result.current[0]).toBe(0);
-  // expect(result.current[4]).toBe(0);
+const store = createStore(
+  reducer,
+  initialState,
+)
 
-  act(() => {
-    result.current[2]([])
+// return [player, updatePlayerPos, resetPlayer, playerRotate];
+
+describe('<usePlayer/> HOOK', () => {
+  const { result, waitForNextUpdate } = renderHook(() => usePlayer(), {
+    wrapper: ({ children }) => <Provider store={store} >{children}</Provider>
+  });
+  it('should not error', () => {
+    expect(result.current[0]).toStrictEqual({"collided": false, "pos": {"x": 3, "y": 0}, "tetromino": [
+      ["I", "I", "I", "I"],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0]
+    ]})
+  });
+  
+  it('new position player', async () => {
+    act(() => {
+      result.current[1]({ x: 0, y: 1, collided: true})
+    })
+    await waitForNextUpdate
+
+    // expect(result.current[0]).toStrictEqual({"collided": true, "pos": {"x": 0, "y": 1}, "tetromino": [
+    //   ["I", "I", "I", "I"],
+    //   [0, 0, 0, 0],
+    //   [0, 0, 0, 0],
+    //   [0, 0, 0, 0]
+    // ]})
+  });
+
+  it('new reset player', async () => {
+    act(() => {
+      result.current[2]([
+        [0, "J", 0],
+        [0, "J", 0],
+        ["J", "J", 0]
+    ])
+    })
+    await waitForNextUpdate
+
+    // expect(result.current[0]).toStrictEqual({"collided": true, "pos": {"x": 0, "y": 1}, "tetromino": [
+    //   [0, "J", 0],
+    //   [0, "J", 0],
+    //   ["J", "J", 0]
+    // ]})
   })
-  // expect(result.current[4]).toBe(1);
-});
 
-// it('should useGameStatus with 40 score', () => {
-//   const { result } = renderHook(() => useGameStatus(1));
+  it('new player rotate', async () => {
+    act(() => {
+      result.current[3]([
+        [0, "J", 0],
+        [0, "J", 0],
+        ["J", "J", 0]
+    ])
+    })
+    await waitForNextUpdate
 
-//   expect(result.current[0]).toBe(40);
-// });
+    // expect(result.current[0]).toStrictEqual({"collided": true, "pos": {"x": 0, "y": 1}, "tetromino": [
+    //   [0, "J", 0],
+    //   [0, "J", 0],
+    //   ["J", "J", 0]
+    // ]})
+  })
+})
---
+++ src/client/hook/tests/useStage.spec.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import { useStage } from '../useStage'
+import { renderHook, act } from '@testing-library/react-hooks';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import reducer from '../../reducers'
+import { createStage } from '../../gameHelper'
+
+const initialState = {
+  game : {
+    shapes : [
+      {
+        shape: [
+          ["I", "I", "I", "I"],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0]
+        ]
+      }
+    ]
+  }
+}
+
+const store = createStore(
+  reducer,
+  initialState,
+)
+
+describe('<useStage/> HOOK', () => {
+  const player = {
+    pos: { x: 10 / 2 - 2, y: 0 },
+    tetromino: [
+      ["I", "I", "I", "I"],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0]
+    ],
+    collided: false,
+  }
+  const newPlayer = jest.fn()
+  const resetPlayer = jest.fn()
+  const { result, rerender } = renderHook(() => useStage(player, newPlayer, resetPlayer), {
+    wrapper: ({ children }) => <Provider store={store} >{children}</Provider>
+  });
+  it('should not error', () => {
+    // expect(result.current[0]).toStrictEqual({"collided": false, "pos": {"x": 3, "y": 0}, "tetromino": [
+    //   ["I", "I", "I", "I"],
+    //   [0, 0, 0, 0],
+    //   [0, 0, 0, 0],
+    //   [0, 0, 0, 0]
+    // ]})
+    expect(result)
+  });
+
+  it('new stage', async () => {
+    
+    act(() => {
+      result.current[1](createStage())
+    })
+  })
+
+  it('new rows cleared', async () => {
+    player.collided = true
+    const { result, rerender } = renderHook(() => useStage(player, newPlayer, resetPlayer), {
+      wrapper: ({ children }) => <Provider store={store} >{children}</Provider>
+    });
+    act(() => {
+      result.current[2]
+    })
+  })
+
+})
